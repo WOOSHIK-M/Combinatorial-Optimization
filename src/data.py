df4f7db7fc227e6393ae7bb7f9b882a2fae5a0d6
wooshik-m <wooshik.m@gmail.com>
2022-08-14 22:21:37 +0900

    Split and merge methods to enhance readability

--- src/data.py
+++ src/data.py
@@ -14,7 +14,7 @@ import re
 import zipfile
 from abc import ABCMeta, abstractmethod
 from pathlib import Path, PosixPath
-from typing import Any, List, Tuple
+from typing import Any, List
 
 import numpy as np
 
@@ -22,6 +22,11 @@ from structure import TSP, City
 
 
 class DataParser(metaclass=ABCMeta):
+    """Parse the given problem.
+
+    If you want to define a new problem and the data is constructed,
+    the data files need to be zipped as {problem_name}.zip.
+    """
 
     DATA_DIR = "data"
     ZIP_EXTENSION = ".zip"
@@ -62,8 +67,11 @@ class TSPDataParser(DataParser):
             if file.suffix == ".tsp"
         ]
 
+        problems = []
         for file in sorted(files):
             tsp = self._parse_tsp_file(self.data_path.joinpath(f"{file}.tsp"))
+            problems.append(tsp)
+        return problems
 
     def _parse_tsp_file(self, file_path: PosixPath) -> TSP:
         """Parse data from .tsp file."""
@@ -71,69 +79,55 @@ class TSPDataParser(DataParser):
 
         with open(file_path, "r") as file:
             contents = file.read()
+            name = re.findall(r"NAME\s*:\s*(.+)\n", contents)[0]
+            n_cities = int(re.findall(r"DIMENSION\s*:\s*(.+)\n", contents)[0])
 
-            # parse data
+            # parse cities
             if "NODE_COORD_SECTION" in contents:
-                tsp = self._parse_node_coord_section(contents)
+                city_block = self._get_city_block("NODE_COORD_SECTION", contents)
             elif "DISPLAY_DATA_SECTION" in contents:
-                tsp = self._parse_display_data_section(contents)
+                city_block = self._get_city_block("DISPLAY_DATA_SECTION", contents)
             else:
                 raise NotImplementedError(f"Unknown format...{file_path}")
-        return tsp
 
-    def _parse_node_coord_section(self, contents: str) -> TSP:
-        """Parse cities if the file contains node coordinates."""
-        name = re.findall(r"NAME\s*:\s*(.+)\n", contents)[0]
-        n_cities = int(re.findall(r"DIMENSION\s*:\s*(.+)\n", contents)[0])
+            cities = self._parse_cities(city_block)
+            assert n_cities == len(cities), "Wrong regex to parse .tsp file..."
 
+            adj_mat = self._make_adjacency_matrix(cities)
+        return TSP(name=name, adj_mat=adj_mat)
+
+    def _get_city_block(self, section_name: str, contents: str) -> str:
+        """Get city informations of the given section."""
+        pattern = rf"{section_name}\s*(\n.+)+(\nEOF)*"
+        city_block = re.search(pattern, contents).group().split("\n")
+        if "EOF" in city_block[-1]:
+            city_block.pop()
+        return city_block
+
+    def _parse_cities(self, city_block: str) -> List[City]:
+        """Parse cities from the given text.
+
+        It has the below format.
+            {idx} {x_coordinate} {y_coordinate}
+            ...
+        """
         cities = []
-        city_block = re.search(
-            r"NODE_COORD_SECTION\s*(\n.+)+(\nEOF)*", contents
-        ).group()
-        city_blocks = city_block.split("\n")
-        if "EOF" in city_blocks[-1]:
-            city_blocks.pop()
-
-        for line in city_blocks[1:]:
-            city_idx, x, y = line.split()
-            city = City(index=int(city_idx), x=float(x), y=float(y))
+        for line in city_block[1:]:
+            city_idx, loc_x, loc_y = line.split()
+            city = City(index=int(city_idx), x=float(loc_x), y=float(loc_y))
             cities.append(city)
-        assert n_cities == len(cities), "Wrong regex to parse .tsp file..."
+        return cities
 
+    def _make_adjacency_matrix(self, cities: List[City]) -> np.ndarray:
+        """."""
+        n_cities = len(cities)
         adj_mat = np.zeros((n_cities, n_cities))
         # for i, city_i in enumerate(cities[:-1]):
         #     for j, city_j in enumerate(cities[i + 1 :], start=i + 1):
         #         weight = abs(city_i.x - city_j.x) + abs(city_i.y - city_j.y)
         #         adj_mat[i, j] = weight
         #         adj_mat[j, i] = weight
-        return TSP(name=name, adj_mat=adj_mat)
-
-    def _parse_display_data_section(self, contents: str) -> TSP:
-        """Parse cities if the file contains display data."""
-        name = re.findall(r"NAME\s*:\s*(.+)\n", contents)[0]
-        n_cities = int(re.findall(r"DIMENSION\s*:\s*(.+)\n", contents)[0])
-
-        display_data_section = re.search(
-            r"DISPLAY_DATA_SECTION\s*(\n.+)+(\nEOF)*", contents
-        ).group()
-        city_blocks = display_data_section.split("\n")
-        if "EOF" in city_blocks[-1]:
-            city_blocks.pop()
-
-        cities = []
-        for line in display_data_section.split("\n")[1:-1]:
-            city_idx, x, y = line.split()
-            city = City(index=int(city_idx), x=float(x), y=float(y))
-            cities.append(city)
-
-        adj_mat = np.zeros((n_cities, n_cities))
-        # for i, city_i in enumerate(cities[:-1]):
-        #     for j, city_j in enumerate(cities[i + 1 :], start=i + 1):
-        #         weight = abs(city_i.x - city_j.x) + abs(city_i.y - city_j.y)
-        #         adj_mat[i, j] = weight
-        #         adj_mat[j, i] = weight
-        assert n_cities == len(cities), "Wrong regex to parse .tsp file..."
-        return TSP(name=name, adj_mat=adj_mat)
+        return adj_mat
 
     def _parse_tour(self, file_path: PosixPath) -> TSP:
         """Parse data from .tour file."""
